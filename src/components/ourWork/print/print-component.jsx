27d8bf0b7e96c7b870fdcc7dfa7d1a1dd433d9fe
salmanowes2022 <salman.awasise@students.alquds.edu>
2022-11-08 18:57:27 +0200

    last edit
    print & website

--- src/components/ourWork/print/print-component.jsx
+++ src/components/ourWork/print/print-component.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import './prints.css'
+import Post from './img-3/Business card.png'
+
+import HasbaraPost from './img-3/Pic 1.png'
+import HasbaraPost2 from './img-3/Pic 2.png'
 
 import Button from '../../button/button-component'
 import Header from '../header/workHeader-component'
@@ -6,10 +11,46 @@ import Example from '../example/workExample-component'
 
 function PrintComponent(props) {
     return (
-        <div>
-            Print
+        <div className='prints'>
+            <Header header='prints' des='Business cards, books, notebooks, etc.' />
+            <div className="print-examples">
+                <Example 
+                    first='Example of a business card for “Coach”.' 
+                    second='A fitness trainer who specializes in a healthy lifestyle.' 
+                    third='We decided to use authoritative colors, a friendly font and an aesthetic that represents cleanliness.' 
+                />
+                <div className="print-two-pictures">
+                    <img className='print-post' src={Post} alt="" />
+                    
+                </div>
+                <Example 
+                    first='Example of a products for “Orbach” -' 
+                    second='Law firm.' 
+                    third='We chose the color gray because there is always a gray area between justice and injustice. The products are white to represent optimism.' 
+                />
+                <div className="print-pictures">
+                    <img className='print-hasbara' src={HasbaraPost} alt="" />
+                    <img className='print-hasbara' src={HasbaraPost2} alt="" />
+
+                </div>
+               
+            </div>
+            <div className="print-button"><Button text='NEXT' /></div>
         </div>
     );
 }
 
 export default PrintComponent;
+
+
+
+// Example of a business card for “Coach”.
+// A fitness trainer who specializes in a healthy lifestyle.
+// We decided to use authoritative colors, a friendly font and an aesthetic that represents cleanliness.
+
+
+
+
+// Example of a products for “Orbach” -
+// Law firm.
+// We chose the color gray because there is always a gray area between justice and injustice. The products are white to represent optimism.
--- src/components/ourWork/websites/websites-component.jsx
+++ src/components/ourWork/websites/websites-component.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import './website.css'
+import pic1 from './img-2/Collection SS22.jpg'
+import pic2 from './img-2/Hompage.jpg'
+
+
+
 
 import Button from '../../button/button-component'
 import Header from '../header/workHeader-component'
@@ -6,10 +12,33 @@ import Example from '../example/workExample-component'
 
 function WebsitesComponent(props) {
     return (
-        <div>
-            Websites
+        <div className='website'>
+            <Header header='website' des='On Wordpress, Elementor, Wix, html - css' />
+            <div className="website-examples">
+                <Example 
+                    first=' An example of a website for Medina Studio.' 
+                    second='A site that is both a portfolio and an ecommerce together.' 
+                    third='The challenge was to combine both, while presenting an impressive display of the images and a user-friendly characterization.' 
+                />
+                <div className="website-one-pictures">
+                    <img className='website-post' src={pic2} alt="" />
+                    
+                </div>
+
+                <div className="website-pictures">
+                    <img className='website-hasbara' src={pic1} alt="" />
+                </div>
+               
+            </div>
+            <div className="website-button"><Button text='NEXT' /></div>
         </div>
     );
 }
 
 export default WebsitesComponent;
+
+
+
+// An example of a website for Medina Studio.
+// A site that is both a portfolio and an ecommerce together.
+// The challenge was to combine both, while presenting an impressive display of the images and a user-friendly characterization.
