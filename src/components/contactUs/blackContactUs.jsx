ee20475a12bf4ac467e579ae297275957ce72798
omarobaisi <omr.obasi28@gmail.com>
2022-11-21 17:05:55 +0200

    c and logo

--- src/components/contactUs/blackContactUs.jsx
+++ src/components/contactUs/blackContactUs.jsx
@@ -4,6 +4,17 @@ import './Contact.scss'
 import './BlackContact.scss'
 import Button from '../button/button3/button3-component'
 
+import c1920 from './img/Desktop/1920w/c.png'
+import c1728 from './img/Macbook/1728w/c.png'
+import c1512 from './img/Macbook/1512w/c.png'
+import c1440 from './img/Desktop/1440w/c.png'
+import c1280 from './img/Macbook/1280w/c.png'
+import c1024 from './img/Tablet/1024w/c.png'
+import c834 from './img/Tablet/834w/c.png'
+import c430 from './img/iPhone/430w/c.png'
+import c390 from './img/iPhone/390w/c.png'
+import c360 from './img/Android/c.png'
+
 // npm i @emailjs/browser
 
 const ContactUs = () => {
@@ -44,7 +55,19 @@ const ContactUs = () => {
 				<div className="contact-button black-contact-button"><Button text='SEND' /></div>
 			</div>
 		</div>
-		<div><p><i className="fa-regular fa-copyright"></i> 2022 onwebs. All rights reserved.</p></div>
+		<div>
+			<div className='img-1920'><p><img src={c1920} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-1728'><p><img src={c1728} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-1512'><p><img src={c1512} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-1440'><p><img src={c1440} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-1280'><p><img src={c1280} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-1024'><p><img src={c1024} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-834'><p><img src={c834} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-430'><p><img src={c430} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-390'><p><img src={c390} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			<div className='img-360'><p><img src={c360} alt="" /> 2022 onwebs. All rights reserved.</p></div>
+			{/* <p><i className="fa-regular fa-copyright"></i> 2022 onwebs. All rights reserved.</p> */}
+		</div>
 	</div>
   );
 };
