8b1fb5c17888e55deb8e307ac18abe6c43c39925
omarobaisi <omr.obasi28@gmail.com>
2022-12-01 17:18:39 +0200

    mobile quote

--- src/components/home/quote/quote-component.jsx
+++ src/components/home/quote/quote-component.jsx
@@ -4,7 +4,13 @@ import './Quote.scss'
 function Quote(props) {
     return (
         <div className='quote'>
-            <div className="the-quote">"The studio took us to a smarter, right and of course beautiful area."</div>
+            <div className="the-quote desktop-quote">"The studio took us to a smarter, right and of course beautiful area."</div>
+            <div className="the-quote mobile-quote">
+                <div>"The studio took us</div>
+                <div>to a smarter,</div>
+                <div>right and of course</div>
+                <div>beautiful area."</div>
+            </div>
             <div className="author">Aviv Rosenblum, Cobwebs</div>
         </div>
     );
